d9e76610ca404f43c4bf9681b394b9e998ffdd75
Jessica Hawkins <jessicarhawkins08@outlook.com>
2016-03-12 02:44:06 -0600

    Add files for first time

---
+++ assets/scripts/main.js
@@ -0,0 +1,54 @@
+(function($){
+  $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    new WOW().init();
+    
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+    
+    $('#collapsingNavbar li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggler:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+    $("#collapse-bs").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    // Page scrolling for #page-scroll li classes in navbar
+
+  $('.arrow').click(function () {
+    $('body, html').animate({
+      scrollTop: $('#portfolio').offset().top
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       100,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        }
+      }
+    );
+    wow.init();
+})(this.jQuery);

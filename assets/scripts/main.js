d278fe3e877da6f5c1dbeee1f7e3a3a0aad2a4c5
Jessica Hawkins <jessicarhawkins08@outlook.com>
2016-07-09 08:11:29 -0500

    update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,21 @@
-(function($){
-  $('body').scrollspy({
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 60
     });
 
     new WOW().init();
     
-    $('a.page-scroll').bind('click', function(event) {
-        var $ele = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top - 60)
-        }, 1450, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+                var e = t(this.hash);
+                if (e = e.length ? e : t("[name=" + this.hash.slice(1) + "]"), e.length) return t("html, body").animate({
+                    scrollTop: e.offset().top - t("#sticky").height()
+                }, 1e3), !1
+            }
+  });
     
     $('#collapsingNavbar li a').click(function() {
         /* always close responsive nav after click */
@@ -23,32 +26,4 @@
        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
     });
 
-    $("#collapse-bs").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-
-    // Page scrolling for #page-scroll li classes in navbar
-
-  $('.arrow').click(function () {
-    $('body, html').animate({
-      scrollTop: $('#portfolio').offset().top
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
-  var wow = new WOW(
-      {
-        boxClass:     'wow',      // animated element css class (default is wow)
-        animateClass: 'animated', // animation css class (default is animated)
-        offset:       100,          // distance to the element when triggering the animation (default is 0)
-        mobile:       true,       // trigger animations on mobile devices (default is true)
-        live:         true,       // act on asynchronously loaded content (default is true)
-        callback:     function(box) {
-          // the callback is fired every time an animation is started
-          // the argument that is passed in is the DOM node being animated
-        }
-      }
-    );
-    wow.init();
-})(this.jQuery);
+})(jQuery);
